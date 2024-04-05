367b9416f086d7e31ad8ba1b6366bd2845ecb4fb
prkhar <prkharpatel20@gmail.com>
2024-04-05 23:58:31 +1100

    added subnets for all components

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,22 +1,53 @@
+#VPC
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
-
 
   tags = {
     Name = "${var.env}-vpc"
   }
 }
 
+#SUBNETS
+
 resource "aws_subnet" "frontend_subnet" {
-  count = length(var.frontend_subnet)
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.frontend_subnet[count.index]
+  count             = length(var.frontend_subnet)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.frontend_subnet[count.index]
   availability_zone = var.availability_zone[count.index]
+
   tags = {
     Name = "${var.env}-frontend_subnet-${count.index+1}"
   }
 
 }
+
+
+resource "aws_subnet" "backend_subnet" {
+  count             = length(var.backend_subnet)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.backend_subnet[count.index]
+  availability_zone = var.availability_zone[count.index]
+
+  tags = {
+    Name = "${var.env}-backend_subnet-${count.index+1}"
+  }
+
+}
+
+
+resource "aws_subnet" "db_subnet" {
+  count             = length(var.db_subnet)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.db_subnet[count.index]
+  availability_zone = var.availability_zone[count.index]
+
+  tags = {
+    Name = "${var.env}-db_subnet-${count.index+1}"
+  }
+
+}
+
 
 resource "aws_vpc_peering_connection" "peering_connection" {
   peer_vpc_id = var.default_vpc_Id   #Acceptor
